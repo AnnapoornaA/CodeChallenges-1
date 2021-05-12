ede43f9afbab3d5b5cd3a153c66f59954870046c
AnnaapoornA <Aannapoorna.Akshintala@gmail.com>
2021-05-12 15:48:26 +0530

    input

--- CSharpUnitTestChallenge/TestProject1/Input.cs
+++ CSharpUnitTestChallenge/TestProject1/Input.cs
@@ -12,7 +12,7 @@ namespace TestProject1
     {
         public static MonthlyBudget ActualValues()
         {
-            MonthlyBudget actual=new MonthlyBudget();
+            var actual=new MonthlyBudget();
             actual.FirstName = "Anna";
             actual.LastName = "Phelange";
             actual.DateOfBirth = Convert.ToDateTime("01/01/1984");
